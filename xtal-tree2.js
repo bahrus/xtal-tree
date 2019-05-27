6df837ce969a6d2b6d1a56388d6e224084c9a16f
bahrus <anderson.bruce.b@gmail.com>
2019-05-27 12:18:29 -0400

    ...

--- xtal-tree2.js
+++ xtal-tree2.js
@@ -1,12 +1,27 @@
 import { decorate } from 'trans-render/decorate.js';
 export function XtalTreeDeco(root) {
+    console.log('decorating');
     decorate(root, {
         propDefs: {
-            allExpanded: false
+            allExpanded: false,
+            allCollapsed: false,
         },
         methods: {
-            onPropsChange(name, oldVal, newVal) {
-                debugger;
+            onPropsChange(name, newVal) {
+                switch (name) {
+                    case 'allExpanded':
+                        if (newVal) {
+                            this.setAttribute('open', '');
+                            this.querySelectorAll('details').forEach(details => details.setAttribute('open', ''));
+                        }
+                        this.allCollapsed = false;
+                        break;
+                    case 'allCollapsed':
+                        if (newVal) {
+                            this.removeAttribute('open');
+                            this.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
+                        }
+                }
             }
         }
     });

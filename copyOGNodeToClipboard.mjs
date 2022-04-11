d1f0f9f7181b4d8ae5152e764dc12a48af313252
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-11 15:53:34 -0400

    0.0.132

--- copyOGNodeToClipboard.mjs
+++ copyOGNodeToClipboard.mjs
@@ -2,7 +2,7 @@ import { getOGFromPath } from './getOGFromPath.mjs';
 export function copyOGNodeToClipboard(og, path) {
     const match = getOGFromPath(og, path);
     const { baseValue, prop, idx } = match;
-    const val = baseValue[prop || idx];
+    const val = (prop || idx) === '' ? baseValue : baseValue[prop || idx];
     const json = JSON.stringify(val, null, 2);
     navigator.clipboard.writeText(json);
 }
